5fcfc67d7df5e8fcb911fe832fa05534510c3bf5
Ilya Shurmanov <ilya.shurmanov@gmail.com>
2023-05-21 20:08:59 +0300

    initial commit

---
+++ telegram_bot_main.py
@@ -0,0 +1,22 @@
+import os
+from telegram.ext import Updater, MessageHandler, Filters
+
+# Retrieve the API token from the environment variable
+API_TOKEN = os.environ.get('TELEGRAM_API_TOKEN')
+
+def handle_message(update, context):
+    """Callback function for handling messages"""
+    message = update.message.text
+    if 'Hi' in message:
+        chat_id = update.effective_chat.id
+        context.bot.send_message(chat_id=chat_id, text='Hello')
+
+# Create an Updater instance using the API token
+updater = Updater(token=API_TOKEN)
+
+# Register the callback function for handling messages
+dispatcher = updater.dispatcher
+dispatcher.add_handler(MessageHandler(Filters.text & (~Filters.command), handle_message))
+
+# Start the bot
+updater.start_polling()
